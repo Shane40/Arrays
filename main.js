f02d77243e7a225f525f73f4ec6bbb7862c9c168
Shane Blatt <shane.blatt@yahoo.com>
2015-01-17 19:04:32 -0500

    Question 5 more work

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-// 1. What am I working with?
-// 2. What do I want to do with it? TELL YOURSELF IN A SENTENCE: WHat do I want to do?
-// 3. What's the best way to get that data (what kind of forEach)
-//4. Define the function?
-//5. Decide what you want
-
-
-
-	// What I want console to do:
-	// 	1. Go down the list of items
-	// 	2. Find price
-	// 	3. Call up the price
-	// 	4. Average the price
-	// 	5. Spit it out
+//Chelsea's suggestions
+// 1. Figure out what working with.
+// 2. What do I want to do with it? TELL YOURSELF IN A SENTENCE: What do I want to do?
+// 3. What's the best way to get that data (what kind of forEach)?
+// 4. Define the function.
+// 5. Decide what you want the function to do.
+// 6. Console log the outcome.
 
 
 //QUESTION 1
 // Show me how to calculate the average price of all items. 
 // Please console.log the average.
+// What I want console to do:
+	// 	1. Go down the list of items
+	// 	2. Find price
+	// 	3. Call up the price
+	// 	4. Average the price
+	// 	5. Spit it out
 
 var emptyArray = [];
 console.log(emptyArray);
@@ -83,7 +82,30 @@ var wood = items.filter ( function (object){
 //Show me how to find 
 //which items are made of eight or more materials. Please console.log the ones you find.
 
+// What's the question asking: It's asking that within the bucket of items,
+// drill down to materials and see which ones contain eight or more items.
+// Here's the catch: Within Materials objects is an array. So he's really
+// asking that within the Materials object, which ARRAY contains eight or
+// more items.
 
+// //var itemsMaterials = [];
+// items.forEach ( function (item){
+// 	if (item.materials.length >= 7) {
+// 		itemsMaterials.push(item.materials.length);
+// });
+
+// 	itemsMaterials.forEach (function (eight) {
+// 	console.log(eight);
+// 	});
+
+var itemsMaterials = items.filter ( function (object){
+	return object.title.length >= 7;
+
+});
+	console.log(itemsMaterials);
+
+//QUESTION 6
+//Show me how to calculate how many items were made by their sellers.
 
 
 
